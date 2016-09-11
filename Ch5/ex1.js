4fb7e4eaffec6362505bb63a064eecec448a64e0
akwebd <akwebd>
2016-09-12 00:25:19 +0300

    just to recommit

--- Ch5/ex1.js
+++ Ch5/ex1.js
@@ -1,6 +1,6 @@
 //using reduce method in cobination with the concat method to "flatten an array of arrays into a single array that has all the elements of the input arrays.
-var inArray = [[11, 12, 13], [21, 22, 23], [31, 32, 34], 44];
+var arrays = [[1, 2, 3], [4, 5], [6]];
 
-console.log(inArray.reduce(function(a, b){
+console.log(arrays.reduce(function(a, b){
     return a.concat(b);
 }));
--- Ch5/ex2.js
+++ Ch5/ex2.js
@@ -1,3 +1,13 @@
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
 //using data set from this chapter, compute average age difference between mothers and children. Note that not all mothers may be in the data set
 function average(array){
     function plus(a, b){return a + b;}
